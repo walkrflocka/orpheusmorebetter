9ab64e11eaab7b67982c17496ca26dcd574ea4c2
walker smith <89418730+walkrflocka@users.noreply.github.com>
2025-05-26 17:53:21 -0400

    model changes

--- models/torrent.py
+++ models/torrent.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel
 
 class Torrent(BaseModel):
     id: int
-    groupId: int
+    groupId: int | None
     media: str
     format: str
     seeders: int
     snatched: int
     remastered: bool
     description: str
-    remasterYear: str
+    remasterYear: int
     remasterTitle: str
     remasterRecordLabel: str
     remasterCatalogueNumber: str
