6449c11ae9276d33de76697703e85a4de7ce9895
marmalade7586 <anvil-rental-guru@duck.com>
2025-12-21 13:54:06 -0500

    Create test_tracknumbers.py

---
+++ tests/test_tracknumbers.py
@@ -0,0 +1,9 @@
+from services import tagging
+
+class TrackNumTester:
+    def test_single(self):
+        assert tagging.format_track_and_disc_numbers("1","2") == "1/2"
+        assert tagging.format_track_and_disc_numbers("10/2","2") == "10/2"
+        assert tagging.format_track_and_disc_numbers("A1/3","4") == "A1/4"
+        assert tagging.format_track_and_disc_numbers("A1","2") == "A1/2"
+        assert tagging.format_track_and_disc_numbers("1/2","") == "1/2"
